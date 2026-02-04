3a0492ad5e4d1d1f8fc1da6a9d34c0d8f8bcc7f5
AldinaIsmic <aldina.ismic.22@size.ba>
2026-02-04 23:06:24 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,21 +12,20 @@
 
     const app = express();
 
-    const corsOptions = {
-        origin: [
-            "http://localhost:5173",
-            "https://test.unafilm.ba",
-            "https://unafilm.ba"
-        ],
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    };
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-    app.use(cors(corsOptions));
-    app.options("*", cors(corsOptions));
+        if (req.method === "OPTIONS") {
+            return res.sendStatus(200);
+        }
+
+        next();
+    });
+
     app.use(express.json());
 
-    console.log("CORS VERSION 2026-02-04 A");
 
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
