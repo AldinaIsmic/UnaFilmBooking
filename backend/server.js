48db831f40bfa27c27f19bbb6a251b89c714bcdb
AldinaIsmic <aldina.ismic.22@size.ba>
2026-02-04 14:14:28 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@
     const app = express();
 
     app.use(cors({
-        origin: "http://localhost:5173"
+        origin: [
+            "http://localhost:5173",
+            "https://test.unafilm.ba"
+        ],
+        credentials: true
     }));
     app.use(express.json());
 
